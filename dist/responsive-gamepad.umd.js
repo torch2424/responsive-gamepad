177b7d9d3f24e5d396476a4b49921112e5b9013b
Aaron Turner <aaron@aaronthedev.com>
2018-03-08 23:26:24 -0800

    Got the build system/environment working

---
+++ dist/responsive-gamepad.umd.js
@@ -0,0 +1,13 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.ResponsiveGamepad = {})));
+}(this, (function (exports) { 'use strict';
+
+var test = "Testing";
+
+exports.test = test;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
