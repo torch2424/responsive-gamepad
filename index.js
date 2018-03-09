9f5ac3b1bf490824764a4bafeeb32a2c82ccb123
Aaron Turner <aaron@aaronthedev.com>
2018-03-09 01:43:27 -0800

    Started the demopage

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+import './style';
+import { Component } from 'preact';
 import { ResponsiveGamepad } from './dist/responsive-gamepad.esm';
+
+export default class App extends Component {
+
+	// Using componentDidMount to wait for the canvas element to be inserted in DOM
+	componentDidMount() {
+    // Initialize our gamepad
+		ResponsiveGamepad.initialize();
+	}
+
+	render() {
+		return (
+			<div>
+        hello!
+			</div>
+		);
+	}
+}
